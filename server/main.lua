e25be66c8448d62a6d1711ef1acf212eeb1e7fa1
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2026-02-03 22:28:59 +1100

    feat(server/main): init case rewards

--- server/main.lua
+++ server/main.lua
@@ -6,42 +6,48 @@ local CONFIG <const> = glib.require(RES_NAME..'.server.config') --[[@module 'gr_
 local LOCK_COOLDOWN <const> = CONFIG.cooldowns.locks * 60
 local CASE_COOLDOWN <const> = CONFIG.cooldowns.cases * 60
 local ALARM_COOLDOWN <const> = CONFIG.cooldowns.alarm * 60
+local OPEN_HOUR <const> = CONFIG.hours.open
+local CLOSE_HOUR <const> = CONFIG.hours.close
+local AUTOLOCK <const> = CONFIG.autolock
+local REWARDS <const> = CONFIG.rewards
 local Cases = {}
 local Stores = {}
 local PresenceCache = {}
 local Cooldowns = {}
 
-local TimeOuts = {
-  [1] = false,
-  [2] = false,
-  [3] = false
-}
-
-local CachedPoliceAmount = {}
-local Flags = {}
-
-
 -------------------------------- FUNCTIONS --------------------------------
 
-local function set_door_state(location, _type, state)
+---@param location string
+---@param _type string
+---@param state boolean
+---@param and_sec boolean?
+local function set_door_state(location, _type, state, and_sec)
   local src = source ~= -1 and source or GetPlayers()[1]
   if _type == 'hit' then
-  local doors = LOCATIONS[location].doors
+  local doors = LOCATIONS[location]?.doors
   if not doors then return end
-  for i = 1, #doors do
-    local door = doors[i]
-    bridge.doorlock.setstate(src, door, not state)
+  bridge.doorlock.setstate(src, doors[1], not state)
+  Stores[location].locked = not state
+  if and_sec then
+    bridge.doorlock.setstate(src, doors[2], not state)
   end
 elseif _type == 'hacked' then
-  for _, v in pairs(LOCATIONS) do
+  for k, v in pairs(LOCATIONS) do
     if v.doors then
       for i = 1, #v.doors do
         local door = v.doors[i]
         bridge.doorlock.setstate(src, door, not state)
       end
+      Stores[k].locked = true
     end
   end
 end
+end
+
+---@return boolean open
+local function is_store_open()
+  local hour = bridge.weather.gettime()
+  return hour >= OPEN_HOUR and hour < CLOSE_HOUR
 end
 
 ---@async
@@ -50,12 +56,20 @@ local function main_thread()
   while true do
     Wait(1000)
     for location, _types in pairs(Cooldowns) do
-      if _types.locks then
+      if _types.locks and _types.locks ~= 0 then
         _types.locks -= 1
         if _types.locks == 0 then
           set_door_state(location, 'hit', false)
           Stores[location].hit = false
           Stores[location].hacked = false
+        end
+      elseif AUTOLOCK then
+        if not is_store_open() then
+          if not Stores[location].locked then
+            set_door_state(location, 'hit', false, true)
+          end
+        elseif Stores[location].locked then
+          set_door_state(location, 'hit', true, false)
         end
       end
       for i = 1, #_types.cases do
@@ -87,7 +101,8 @@ local function init_script(resource)
       coords = LOCATIONS[location] and LOCATIONS[location].coords,
       police = LOCATIONS[location] and LOCATIONS[location].police,
       hit = false,
-      hacked = false
+      hacked = false,
+      locked = false
     }
     Cooldowns[location] = {locks = false, cases = {}, alarm = false}
     for i = 1, #data do
@@ -109,6 +124,7 @@ local function init_script(resource)
   end)
 end
 
+---@param resource string
 local function deinit_script(resource)
   if resource ~= RES_NAME then return end
   Cases = {}
@@ -116,6 +132,18 @@ local function deinit_script(resource)
   PresenceCache = {}
   Cooldowns = {}
   GlobalState['jewellery:alarm'] = false
+end
+
+---@param player string|integer
+local function give_case_reward(player)
+  local reward = REWARDS[math.random(#REWARDS)]
+  local item = reward.item
+  local amount = type(reward.amount) == 'table' and math.random(reward.amount.min, reward.amount.max) or reward.amount
+  if bridge.core.addplayeritem(player, item, amount) then
+    bridge.notify.item(player, item, amount)
+  else
+    bridge.notify.text(player, Lang:t('error.too_much'), 'error')
+  end
 end
 
 ---@param location string
@@ -133,7 +161,7 @@ local function set_case_state(location, case, _type, state)
     Cooldowns[location].cases[case] = state and CASE_COOLDOWN
     TriggerClientEvent('jewellery:client:SetCaseState', -1, location, case, state)
     if not state then return end
-    -- Reward Player
+    give_case_reward(src)
   end
 end
 
@@ -158,6 +186,11 @@ local function set_store_state(location, _type, state)
   if not PresenceCache[src] then return end -- Triggered without using target
   if not LOCATIONS[location] then return end
   if #(LOCATIONS[location].coords - GetEntityCoords(GetPlayerPed(src))) > 100.0 then return end
+  if _type == 'hit' and state then
+    if not bridge.core.doesplayerhaveitem(src, 'thermite') then return end -- Triggered without item, definitely cheating
+    bridge.core.removeplayeritem(src, 'thermite', 1)
+    bridge.notify.item(src, 'thermite', -1)
+  end
   Stores[location][_type] = state
   Cooldowns[location].locks = state and LOCK_COOLDOWN
   set_door_state(location, _type, state)
@@ -211,32 +244,6 @@ local function is_store_vulnerable(player, location)
   return store.hacked, store.hit
 end
 
-local function randomNum(min, max)
-  math.randomseed(os.time())
-  local num = math.random() * (max - min) + min
-  if num % 1 >= 0.5 and math.ceil(num) <= max then
-    return math.ceil(num)
-  end
-  return math.floor(num)
-end
-
-local function exploitBan(id, reason)
-  MySQL.insert('INSERT INTO bans (name, license, discord, ip, reason, expire, bannedby) VALUES (?, ?, ?, ?, ?, ?, ?)',
-    {
-      GetPlayerName(id),
-      bridge.core.getplayeridentifier(id),
-      ('discord:' .. (GetPlayerIdentifierByType(id, 'discord') or 'N/A')),
-      GetPlayerIdentifierByType(id, 'ip') or 'N/A',
-      reason,
-      2147483647,
-      'don-jewellery'
-    }
-  )
-  TriggerEvent('qb-log:server:CreateLog', 'jewellery', 'Player Banned', 'red',
-  string.format('%s was banned by %s for %s', GetPlayerName(id), 'don-jewellery', reason), true)
-  DropPlayer(id, 'You were permanently banned by the server for: Exploiting')
-end
-
 -------------------------------- EVENTS --------------------------------
 
 AddEventHandler('onResourceStart', init_script)
@@ -245,184 +252,7 @@ RegisterServerEvent('jewellery:server:SetCaseState', set_case_state)
 RegisterServerEvent('jewellery:server:VangelicoAlarm', set_alarm_state)
 RegisterServerEvent('jewellery:server:SetStoreState', set_store_state)
 
-RegisterServerEvent('don-jewellery:server:RemoveDoorItem', function()
-  local src = source
-  local Player = bridge.core.getplayer(src)
-  local item = Config.DoorItem
-  if not Player then return end
-  bridge.core.removeplayeritem(src, item, 1)
-end)
-
-RegisterServerEvent('don-jewellery:server:SetVitrineState', function(stateType, state, k)
-  if stateType == 'isBusy' and type(state) == 'boolean' and Config.Vitrines[k] then
-    Config.Vitrines[k][stateType] = state
-    TriggerClientEvent('don-jewellery:client:SetVitrineState', -1, stateType, state, k)
-  end
-end)
-
-RegisterServerEvent('don-jewellery:server:StoreHit', function(storeIndex, bool)
-  local src = source
-  local Player = bridge.core.getplayer(src)
-  if not Player then return end
-  TriggerClientEvent('don-jewellery:client:StoreHit', -1, storeIndex, bool)
-  if storeIndex == 'all' then Config.Stores[1].hacked = bool end
-  for i = 1, #Config.Stores do
-    if storeIndex == 'all' then
-      Config.Stores[i].hit = bool
-    else
-      if i == storeIndex then
-        Config.Stores[storeIndex].hit = bool
-      end
-    end
-  end
-end)
-
-RegisterServerEvent('don-jewellery:server:ToggleDoorlocks', function(store, locked, allStores)
-  local src = source
-  if not allStores then
-    if not Config.Stores[store] then return end
-    if Config.DoorLock == 'qb' then
-      TriggerClientEvent('qb-doorlock:client:setState', -1, src, Config.Stores[store]['Doors'].main, locked, src, false, false)
-    elseif Config.DoorLock == 'ox' then
-      local success, door = pcall(function()
-        return exports['ox_doorlock']:getDoorFromName('jewellery_stores ' ..Config.Stores[store]['Doors'].main)
-      end)
-      if success and door then
-        TriggerEvent('ox_doorlock:setState', door.id, locked)
-      end
-    elseif Config.DoorLock == 'cd' then
-      TriggerClientEvent('cd_doorlock:SetDoorState_name', -1, locked, Config.Stores[store]['Doors'].main, 'Jewellery Stores')
-    end
-  else
-    for i = 1, #Config.Stores do
-      if Config.DoorLock == 'qb' then
-        TriggerClientEvent('qb-doorlock:client:setState', -1, src, Config.Stores[i]['Doors'].main, locked, src, false, false)
-        TriggerClientEvent('qb-doorlock:client:setState', -1, src, Config.Stores[i]['Doors'].sec, locked, src, false, false)
-      elseif Config.DoorLock == 'ox' then
-        local success1, main = pcall(function()
-          return exports['ox_doorlock']:getDoorFromName('jewellery_stores ' ..Config.Stores[i]['Doors'].main)
-        end)
-        local success2, sec = pcall(function()
-          return exports['ox_doorlock']:getDoorFromName('jewellery_stores ' ..Config.Stores[i]['Doors'].sec)
-        end)
-        if success1 and main then
-          TriggerEvent('ox_doorlock:setState', main.id, locked)
-        end
-        if success2 and sec then
-          TriggerEvent('ox_doorlock:setState', sec.id, locked)
-        end
-      elseif Config.DoorLock == 'cd' then
-        TriggerClientEvent('cd_doorlock:SetDoorState_name', -1, locked, Config.Stores[i]['Doors'].main, 'Jewellery Stores')
-        TriggerClientEvent('cd_doorlock:SetDoorState_name', -1, locked, Config.Stores[i]['Doors'].sec, 'Jewellery Stores')
-      end
-    end
-  end
-end)
-
-RegisterServerEvent('don-jewellery:server:VitrineReward', function(vitrineIndex)
-  local src = source
-  local Player = bridge.core.getplayer(src)
-  local cheating = false
-  if not Config.Vitrines[vitrineIndex] or Config.Vitrines[vitrineIndex].isOpened then 
-    exploitBan(src, 'Trying to trigger an exploitable event \"don-jewellery:server:VitrineReward\"') 
-    return 
-  end
-  if not CachedPoliceAmount[src] then DropPlayer(src, 'Exploiting') return end
-
-  local plrPed = GetPlayerPed(src)
-  local plrCoords = GetEntityCoords(plrPed)
-  local vitrineCoords = Config.Vitrines[vitrineIndex].coords
-  if CachedPoliceAmount[src] >= Config.RequiredCops then
-    if plrPed then
-      local dist = #(plrCoords - vitrineCoords)
-      if dist <= 25.0 then
-        Config.Vitrines[vitrineIndex].isOpened = true
-        Config.Vitrines[vitrineIndex].isBusy = false
-        TriggerClientEvent('don-jewellery:client:SetVitrineState', -1, 'isOpened', true, vitrineIndex)
-        TriggerClientEvent('don-jewellery:client:SetVitrineState', -1, 'isBusy', false, vitrineIndex)
-
-        local reward = Config.VitrineRewards[randomNum(1, #Config.VitrineRewards)]
-        local amount = randomNum(reward['Amounts'].min, reward['Amounts'].max)
-        if bridge.core.addplayeritem(src, reward.item, amount) then
-          bridge.notify.item(src, reward.item, amount)
-        else
-          bridge.notify.text(src, Lang:t('error.to_much'), 'error')
-        end
-      else
-        cheating = true
-      end
-    end
-  else
-    cheating = true
-  end
-
-  if cheating then
-    local license = bridge.core.getplayeridentifier(src)
-    if Flags[license] then
-      Flags[license] = Flags[license] + 1
-    else
-      Flags[license] = 1
-    end
-    if Flags[license] >= 3 then
-      exploitBan('Getting flagged many times from exploiting the \"don-jewellery:server:VitrineReward\" event')
-    else
-      DropPlayer(src, 'Exploiting')
-    end
-  end
-end)
-
-RegisterServerEvent('don-jewellery:server:SetTimeout', function(vitrine)
-  local store = 0
-  if vitrine >= 1 and vitrine <= 20 then
-    store = 1
-  elseif vitrine >= 21 and vitrine <= 26 then
-    store = 2
-  elseif vitrine >= 27 and vitrine <= 32 then
-    store = 3
-  end
-  if not TimeOuts[store] then
-    TimeOuts[store] = true
-    TriggerEvent('qb-scoreboard:server:SetActivityBusy', 'jewellery', true)
-    CreateThread(function()
-      Wait(Config.Timeout)
-      Config.Stores[1].hacked = false
-      for i = 1, #Config.Stores do
-        Config.Stores[i].hit = false
-      end
-      TriggerClientEvent('don-jewellery:client:StoreHit', -1, 'all', false)
-      for i = 1, #Config.Vitrines do
-        Config.Vitrines[i].isOpened = false
-        TriggerClientEvent('don-jewellery:client:SetVitrineState', -1, 'isOpened', false, i)
-        TriggerClientEvent('don-jewellery:client:SetAlertState', -1, false)
-        TriggerEvent('qb-scoreboard:server:SetActivityBusy', 'jewellery', false)
-      end
-      TimeOuts[store] = false
-    end)
-  end
-end)
-
 -------------------------------- CALLBACKS --------------------------------
-
-bridge.callback.register('don-jewellery:server:GetCops', function(source)
-  local src = source
-	local amount = 0
-  for _, playerId in ipairs(GetPlayers()) do
-    local id = tonumber(playerId)
-    if id then
-      local job = bridge.core.getplayerjob(id)
-      if job and bridge.core.doesplayerhavegroup(id, 'leo') then
-        amount = amount + 1
-      end
-    end
-  end
-  CachedPoliceAmount[src] = amount
-  return amount
-end)
-
-bridge.callback.register('don-jewellery:server:GetJewelleryState', function()
-  local data = {Locations = Config.Vitrines, Hacks = Config.Stores}
-	return data
-end)
 
 bridge.callback.register('jewellery:server:GetCaseStates', function(player)
   if not bridge.core.getplayer(player) then return end
@@ -432,3 +262,4 @@ end)
 bridge.callback.register('jewellery:server:IsCaseBusy', is_case_busy)
 bridge.callback.register('jewellery:server:GetPolicePresence', get_police_presence)
 bridge.callback.register('jewellery:server:IsStoreVulnerable', is_store_vulnerable)
+bridge.callback.register('jewellery:server:IsStoreOpen', is_store_open)
