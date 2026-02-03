9b508ec1cb90c7047d586e598152f97673c8e66b
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2026-02-03 12:25:16 +1100

    refactor(shared/store_locations): redundant vectors

--- shared/store_locations.lua
+++ shared/store_locations.lua
@@ -3,20 +3,16 @@ return {
     label = 'Vangelico\'s Jewellers',
     coords = vector3(-630.5, -237.13, 38.08),
     doors = {'jewellery-citymain', 'jewellery-citysec'},
+    police = 0,
     thermite = {
       coords = vector3(-596.02, -283.7, 50.4),
       heading = 300.0,
-      min_z = 50.12,
-      max_z = 51.32,
-      anim = vector4(-596.02, -283.7, 50.4, 304.5),
-      effect = vector3(-596.02, -283.7, 50.19)
+      size = vector3(0.4, 0.8, 1.2)
     },
     hack = {
       coords = vector3(-631.04, -230.63, 38.06),
       heading = 37.0,
-      min_z = 37.56,
-      max_z = 38.56,
-      anim = vector4(-631.03, -229.55, 39.23, 180.18)
+      size = vector3(0.4, 0.6, 1.0)
     }
   }
 }
